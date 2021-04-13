efb2ab392a8d5f01c58b7687089d86d0404b6334
Martin <mikynecek555@gmail.com>
2021-04-13 20:59:18 +0200

    1. Přidání tabulek

--- knihovna_app/admin.py
+++ knihovna_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(autor)
+admin.site.register(ctenar)
+admin.site.register(kniha)
--- knihovna_app/models.py
+++ knihovna_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
 
-# Create your models here.
+class autor(models.Model):
+    jmeno = models.CharField(max_length=30, null=False, blank=False, verbose_name="Jméno")
+    prijmeni = models.CharField(max_length=30, verbose_name="Přijmení", blank=False, null=True)
+    narozeni = models.DateTimeField(verbose_name="Narození")
+    zivotopis = models.TextField(verbose_name="Životopis autora")
+
+    class Meta:
+        ordering = ["jmeno", "-narozeni"]
+
+    def __str__(self):
+        return self.jmeno
+
+class ctenar(models.Model):
+    jmeno = models.CharField(max_length=30, null=False, blank=False, verbose_name="Jméno")
+    prijmeni = models.CharField(max_length=30, verbose_name="Přijmení", blank=False, null=True)
+    narozeni = models.DateTimeField(verbose_name="Narození")
+
+    class Meta:
+        ordering = ["jmeno", "-narozeni"]
+
+    def __str__(self):
+        return self.jmeno
+
+class kniha(models.Model):
+    isbn = models.CharField(max_length=30, verbose_name="ISBN knihy")
+    titul = models.CharField(max_length=100 , null=False, blank=False, verbose_name="Název díla")
+    zanr = models.CharField(max_length=30, verbose_name="Žánr", blank=False, null=False)
+    vydani = models.DateTimeField(verbose_name="Datum vydání knihy")
+    obsah = models.TextField(verbose_name="Obsah knihy")
+
+
+
+    class Meta:
+        ordering = ["-vydani", "titul"]
+
+    def __str__(self):
+        return self.titul
+
